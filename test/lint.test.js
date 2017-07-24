954cded76c58007e8e394abfb88ec3acc70a29e8
ImAdamTM <adam@adamhartwig.co.uk>
2017-07-24 19:53:00 -0400

    chore(test): Adjust eslint unit test time allowance

--- test/lint.test.js
+++ test/lint.test.js
@@ -3,6 +3,10 @@
 const lint = require('mocha-eslint');
 
 const paths = ['bin', 'test'];
-const options = {};
+const options = {
+  timeout: 10000,
+  slow: 3000,
+  strict: true,
+};
 
 lint(paths, options);
